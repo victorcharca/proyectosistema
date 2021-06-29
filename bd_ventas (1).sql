9c7fa76d957bb8d2dcc060ffd004df7a7bf3b1d1
VICTOR <VICTOR@VICTOR>
2021-06-29 17:20:49 -0400

    proyecto final de emergentes 2

---
+++ bd_ventas (1).sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-12-2020 a las 20:27:58
+-- Versión del servidor: 10.1.35-MariaDB
+-- Versión de PHP: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_ventas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `IdCliente` int(11) UNSIGNED NOT NULL,
+  `Dni` varchar(10) DEFAULT NULL,
+  `Nombres` varchar(244) DEFAULT NULL,
+  `Direccion` varchar(244) DEFAULT NULL,
+  `Estado` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`IdCliente`, `Dni`, `Nombres`, `Direccion`, `Estado`) VALUES
+(17, '2', 'Juan Guerrero Solis', 'Los Alamos', '1'),
+(18, '1', 'Maria Rosas Villanueva', 'Los Laureles 234', '1'),
+(19, '3', 'Andres de Santa Cruz', 'Av. La Frontera 347', '1'),
+(20, '4', 'Andres Mendoza', 'Chosica, Lurigancho', '2'),
+(21, '12989048', 'Omar Corani', 'Av.Ugarte', '17');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_ventas`
+--
+
+CREATE TABLE `detalle_ventas` (
+  `IdDetalleVentas` int(11) UNSIGNED NOT NULL,
+  `IdVentas` int(11) UNSIGNED NOT NULL,
+  `IdProducto` int(11) UNSIGNED NOT NULL,
+  `Cantidad` int(11) UNSIGNED DEFAULT NULL,
+  `PrecioVenta` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `IdEmpleado` int(10) UNSIGNED NOT NULL,
+  `Dni` varchar(10) NOT NULL,
+  `Nombres` varchar(255) DEFAULT NULL,
+  `Telefono` varchar(9) DEFAULT NULL,
+  `Estado` varchar(5) DEFAULT NULL,
+  `User` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`IdEmpleado`, `Dni`, `Nombres`, `Telefono`, `Estado`, `User`) VALUES
+(1, '123', 'Pedro Hernandez', '77252459', '1', 'em45'),
+(2, '123', 'Omar Riquelme', '77252459', '1', 'Om46'),
+(3, '123', 'Omar Corani', '70621739', '1', 'owcg21'),
+(4, '123', 'Juancito Pinto', '75824685', 'Nvo', 'jp12'),
+(5, '777', 'Jorge Luis', '222', '1', 'jorgeAZ');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `IdProducto` int(11) UNSIGNED NOT NULL,
+  `Nombres` varchar(244) DEFAULT NULL,
+  `Precio` double DEFAULT NULL,
+  `Stock` int(11) UNSIGNED DEFAULT NULL,
+  `Estado` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`IdProducto`, `Nombres`, `Precio`, `Stock`, `Estado`) VALUES
+(1, 'Laptop Huawey ', 3000, 15, 'Nvo'),
+(2, 'Mouse Logitech 567', 20, 87, '1'),
+(3, 'Laptop Lenovo Ideapad 520', 800, 80, '10'),
+(4, 'HeadPhones Sony M333', 500, 83, '1'),
+(5, 'Procesador Ryzen 3 and 5  Generation4', 1200, 49, 'Nvo'),
+(6, 'Teclado Gamer Ultralet 27x0', 200, 20, 'Nvo'),
+(7, 'Monitor QBEN', 700, 29, '111');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `IdVentas` int(11) UNSIGNED NOT NULL,
+  `IdCliente` int(11) UNSIGNED NOT NULL,
+  `IdEmpleado` int(10) UNSIGNED NOT NULL,
+  `NumeroSerie` varchar(244) DEFAULT NULL,
+  `FechaVentas` date DEFAULT NULL,
+  `Monto` double DEFAULT NULL,
+  `Estado` varchar(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`IdCliente`);
+
+--
+-- Indices de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD PRIMARY KEY (`IdDetalleVentas`,`IdVentas`,`IdProducto`),
+  ADD KEY `Ventas_has_Producto_FKIndex1` (`IdVentas`),
+  ADD KEY `Ventas_has_Producto_FKIndex2` (`IdProducto`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`IdEmpleado`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`IdProducto`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`IdVentas`),
+  ADD KEY `Ventas_FKIndex1` (`IdEmpleado`),
+  ADD KEY `Ventas_FKIndex2` (`IdCliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `IdCliente` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  MODIFY `IdDetalleVentas` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `IdEmpleado` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `IdProducto` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `IdVentas` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD CONSTRAINT `detalle_ventas_ibfk_1` FOREIGN KEY (`IdVentas`) REFERENCES `ventas` (`IdVentas`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `detalle_ventas_ibfk_2` FOREIGN KEY (`IdProducto`) REFERENCES `producto` (`IdProducto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`IdEmpleado`) REFERENCES `empleado` (`IdEmpleado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ventas_ibfk_2` FOREIGN KEY (`IdCliente`) REFERENCES `cliente` (`IdCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
